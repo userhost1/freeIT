a6901e8b2779314fced8e2cd16ae0ec92776422e
Ivanov Sergey <userhost1@gmail.com>
2020-08-21 21:03:54 +0300

    tictactoe REWORK

--- src/homework03/Task07_summ.java
+++ src/homework03/Task07_summ.java
@@ -3,9 +3,13 @@ package homework03;
 public class Task07_summ {
     public static void main(String[] args) {
 //        7)Напишите программу, определяющую сумму всех нечетных чисел от 1до 99
+
         int result = 0;
-        for (int i = 1; i < 100; i++) {
-            result += i;
+
+        for (int i = 1; i <= 99; i++) {
+            if (i % 2 == 1) {
+                result += i;
+            }
         }
         System.out.println(result);
     }
