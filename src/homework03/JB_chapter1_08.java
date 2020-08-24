b7a3e41ea5619e82874ba2da34719644c1405863
Ivanov Sergey <userhost1@gmail.com>
2020-08-25 00:26:16 +0300

    JB_chapter1_ALL

--- src/homework03/JB_chapter1_08.java
+++ src/homework03/JB_chapter1_08.java
@@ -2,6 +2,10 @@ package homework03;
 
 import java.util.Random;
 
+//  Имеется целове число (задать с помощью Random rand = new Random(); int x =
+//  rand.nextInt() ). Это число – количесво денег в рублях. Вывести это число, добавив к
+//  нему слово «рублей» в правильном падеже.
+
 public class JB_chapter1_08 {
     public static void main(String[] args) {
 
