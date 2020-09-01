caee63c64266dbb2c0f109fc59fe1b739c650d85
Ivanov Sergey <userhost1@gmail.com>
2020-09-01 19:31:21 +0300

    ....

---
+++ src/homework02/Task_02_01.java
@@ -0,0 +1,48 @@
+package homework02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//        В переменную записываем число.
+//        Надо вывести на экран сколько в этом числе цифр и положительное оно или отрицательное.
+//        Например, "это однозначное положительное число".
+//        Достаточно будет определить, является личисло однозначным, "двухзначным или трехзначным и более.
+
+public class Task_02_01 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String number = null;
+
+        try {
+            System.out.println("Введите пожалуйста число.");
+            number = reader.readLine();
+        } catch (IOException e) {
+            System.out.println("Что-то пошло не так!");
+            reader.close();
+        }
+        reader.close();
+
+        if (isNumber(number) && (Integer.parseInt(number) >= 0)) {
+            System.out.format("Это %d-значное положительное число.", number.length());
+        } else if (isNumber(number) && (Integer.parseInt(number) < 0)) {
+            System.out.format("Это %d-значное отрицательное число.", number.length() - 1);
+        } else {
+            System.out.println("Вы ввели не число");
+        }
+
+    }
+
+    public static boolean isNumber(String number) {
+
+        try {
+            int i = Integer.parseInt(number);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+}
