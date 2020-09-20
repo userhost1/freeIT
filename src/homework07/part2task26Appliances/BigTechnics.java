ca2ac08abc0085adbc52a897ef90453bfefc28b7
Ivanov Sergey <userhost1@gmail.com>
2020-09-20 14:44:21 +0300

    homework07.part2task26Appliances

--- src/homework07/part2task26Appliances/BigTechnics.java
+++ src/homework07/part2task26Appliances/BigTechnics.java
@@ -2,8 +2,8 @@ package homework07.part2task26Appliances;
 
 public class BigTechnics extends Appliances {
 
-    int height;
-    int width;
+    protected int height;
+    protected int width;
 
     protected BigTechnics(int height, int weight, String name, String brand, String model, int powerWT) {
         super(name, brand, model, powerWT);
