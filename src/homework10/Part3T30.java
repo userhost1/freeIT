7cefa63f9ee86fdafd32d642d2c2b4346252bd13
Ivanov Sergey <userhost1@gmail.com>
2020-10-25 16:41:16 +0300

    homework 10 task 30 fixed

--- src/homework10/Part3T30.java
+++ src/homework10/Part3T30.java
@@ -11,18 +11,22 @@ public class Part3T30 {
         List<Integer> numbers1 = new ArrayList<>();
         Set<Integer> numbers2 = new TreeSet<>();
 
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 20; i++) {
             numbers1.add((int) (Math.random() * (-10)) + 10);
-            numbers2.add((int) (Math.random() * (-10)) + 10);
+            int num = (int) (Math.random() * (-10) + 10);
+            numbers2.add(num);
+            System.out.print(num + " ");
         }
+        System.out.println();
 
         System.out.println("ArrayList [before removing] " + numbers1);
-        System.out.println("TreeSet [before removing] " + numbers2);
+//        System.out.println("TreeSet [before removing] " + numbers2);
 
         for (int i = 0; i < numbers1.size() - 1; i++) {
-            for (int j = 1; j < numbers1.size(); j++) {
+            for (int j = i + 1; j < numbers1.size(); j++) {
                 if (numbers1.get(i).equals(numbers1.get(j))) {
                     numbers1.remove(j);
+                    j--;
                 }
             }
         }
