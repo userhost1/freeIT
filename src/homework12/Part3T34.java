985c4491ebb4ae66ad3ce9039568e212888a3c9c
Ivanov Sergey <userhost1@gmail.com>
2020-10-31 21:36:26 +0300

    homework12  Part3T33

--- src/homework12/Part3T34.java
+++ src/homework12/Part3T34.java
@@ -13,7 +13,7 @@ import java.util.TreeSet;
 public class Part3T34 {
     public static void main(String[] args) throws IOException {
 
-        int sunDigits = 0;
+        int summ = 0;
         List<Integer> digits = new ArrayList<>();
         Set<Integer> setDigits = new TreeSet<>();
 
@@ -25,7 +25,7 @@ public class Part3T34 {
                 if (Character.isDigit((char) symbol)) {
                     digits.add(Integer.parseInt(String.valueOf((char) symbol)));
                     setDigits.add(Integer.parseInt(String.valueOf((char) symbol)));
-                    sunDigits += Integer.parseInt(String.valueOf((char) symbol));
+                    summ += Integer.parseInt(String.valueOf((char) symbol));
                 }
             }
 
@@ -34,7 +34,7 @@ public class Part3T34 {
         }
 
         System.out.println("Найденные числа: " + digits.toString());
-        System.out.println("Сумма чисел: " + sunDigits);
+        System.out.println("Сумма чисел: " + summ);
         System.out.println("Вывод без повторений: " + setDigits.toString());
 
     }
