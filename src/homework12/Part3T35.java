68945e591ade1136a7795d7b36b3e4e56879c5ca
Ivanov Sergey <userhost1@gmail.com>
2020-11-01 20:29:26 +0300

    homework12  Part3T35 homework12  Part3T36

---
+++ src/homework12/Part3T35.java
@@ -0,0 +1,37 @@
+package homework12;
+
+//  Записать в двоичный файл 20 случайных чисел.
+//  Прочитать записанный файл, распечатать числа и их среднее арифметическое.
+
+import java.io.*;
+
+public class Part3T35 {
+    public static void main(String[] args) throws IOException {
+
+        int count = 0, summa = 0;
+
+        try (DataOutputStream dataWriter = new DataOutputStream(new FileOutputStream("test.txt"));
+             DataInputStream dataReader = new DataInputStream(new FileInputStream("test.txt"))) {
+
+            for (int i = 1; i <= 20; i++) {
+                dataWriter.writeInt(i);
+                dataWriter.flush();
+            }
+            dataWriter.close();
+
+            while (true) {
+                int number = dataReader.readInt();
+                count++;
+                summa += number;
+                System.out.print(number + " ");
+            }
+
+        } catch (IOException e) {
+            System.out.println();
+        }
+
+        System.out.println("count: " + count);
+        System.out.println("summa: " + summa);
+        System.out.println("Среднее арифметическое: " + ((double) summa / count));
+    }
+}
---
+++ src/homework12/Part3T36.java
@@ -0,0 +1,36 @@
+package homework12;
+
+// Вывести список файлов и каталогов выбранного каталога на диске.
+// Файлы и каталоги должны быть распечатаны отдельно.
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Part3T36 {
+    public static void main(String[] args) {
+
+        ArrayList<String> files = new ArrayList<>();
+        ArrayList<String> roots = new ArrayList<>();
+
+        File file = new File("D:\\Download");
+
+        for (File f : Objects.requireNonNull(file.listFiles())) {
+            if (f.isFile()) {
+                files.add(f.getAbsolutePath());
+            } else if (f.isDirectory()) {
+                roots.add(f.getAbsolutePath());
+            }
+        }
+
+        System.out.println("FILES:");
+        for (int i = 0; i < files.size(); i++) {
+            System.out.println(files.get(i));
+        }
+        System.out.println("ROOTS");
+        for (int i = 0; i < roots.size(); i++) {
+            System.out.println(roots.get(i));
+        }
+
+    }
+}
