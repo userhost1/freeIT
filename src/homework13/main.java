b1f9e137e8fd758de959ed48a4df4be7e0ff4c5d
Ivanov Sergey <userhost1@gmail.com>
2021-01-21 23:40:51 +0300

    homework13 XML

---
+++ src/homework13/main.java
@@ -0,0 +1,28 @@
+package homework13;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+
+public class main {
+    public static void main(String[] args) {
+        boolean valid = validatorXMLSchema("D://ONLINE_freeIT//src//homework13/devices.xsd", "D://ONLINE_freeIT//src/homework13/devicesCatalogue.xml");
+        System.out.println("Schema is valid: " + valid);
+    }
+
+    public static boolean validatorXMLSchema(String xsdPath, String xmlPath) {
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File(xsdPath));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(xmlPath)));
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+}
